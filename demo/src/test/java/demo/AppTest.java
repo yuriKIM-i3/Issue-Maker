154bd320e4ae0137402374aa978891b9a21cad69
cheekykorkind <cheekorkind@gmail.com>
2019-05-27 21:19:36 +0900

    todo추가

--- demo/src/test/java/demo/AppTest.java
+++ demo/src/test/java/demo/AppTest.java
@@ -8,7 +8,8 @@ import static org.junit.Assert.*;
 
 public class AppTest {
     @Test public void testAppHasAGreeting() {
-//        App classUnderTest = new App();
-//        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        // @todo : unit테스트 시작하면 작성예정
+        // App classUnderTest = new App();
+        // assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
